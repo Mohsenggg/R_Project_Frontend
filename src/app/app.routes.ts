5b58506635db01d10f23a316ea40cb2cb8deadea
Mohsen_Gamal <mhussen@ntgclarity.com>
2025-04-30 14:20:09 +0300

    Configure Routing In App Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TreeComponent } from './components/tree/tree.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: '', redirectTo:'home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path:'tree', component: TreeComponent},
+
+
+
+];
