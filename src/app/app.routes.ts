54461455831f9bd71b9c1a5c5f2e504d025305ec
Mohsenggg <Mohse.gamal@gmail.com>
2026-01-08 22:10:24 +0200

    refactor || remove unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/intro/home/component/home/home.component';
 import { TreeComponent } from './features/tree/classic-view/component/tree/tree.component';
-import { TreeD3TestComponent } from './features/tree/modern-view/component/tree-d3-test/tree-d3-test.component';
-import { TreePreviewComponent } from './features/tree/preview/component/tree-preview/tree-preview.component';
-import { ViewTreeComponent } from './features/tree/tree/component/view-tree/view-tree.component';
+import { ViewTreeComponent } from './features/tree/dynamic-view/component/view-tree/view-tree.component';
+
 
 export const routes: Routes = [
 
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
       { path: 'tree', component: TreeComponent },
-      { path: 'tree-test', component: TreeD3TestComponent },
-      { path: 'tree-preview', component: TreePreviewComponent },
-
+  
       { path: 'vTree', component: ViewTreeComponent },
-      {
-            path: 'd3-tree',
-            loadComponent: () => import('./features/tree/d3-tree/components/d3-tree/d3-tree.component').then(m => m.D3TreeComponent),
-            data: { demo: true } // optional flag if component needs to know it's a demo
-      },
-
+  
 
 
 ];
--- src/app/features/intro/home/component/home/home.component.ts
+++ src/app/features/intro/home/component/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { TreeComponent } from "../../../../tree/classic-view/component/tree/tree.component";
 import { CommonModule } from '@angular/common';
-import { TreeD3TestComponent } from '../../../../tree/modern-view/component/tree-d3-test/tree-d3-test.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TreeComponent, CommonModule, TreeD3TestComponent],
+  imports: [TreeComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
