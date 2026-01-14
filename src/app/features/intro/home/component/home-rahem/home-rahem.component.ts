dfdd278274a2a9788eab890f5feb6e31f0ac4019
Mohsenggg <Mohse.gamal@gmail.com>
2026-01-15 00:09:41 +0200

    add dynamic display for tree members number

--- src/app/features/intro/home/component/home-rahem/home-rahem.component.ts
+++ src/app/features/intro/home/component/home-rahem/home-rahem.component.ts
@@ -1,15 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TreeDataService } from '../../../../tree/dynamic-view/service/data-access/tree-data.service';
+import { MockTreeDataService } from '../../../../tree/dynamic-view/service/data-access/mock-tree-data.service';
 
 @Component({
       selector: 'app-home-rahem',
       standalone: true,
-      imports: [CommonModule],
+      imports: [CommonModule, RouterLink],
+      providers: [
+            { provide: TreeDataService, useClass: MockTreeDataService }
+      ],
       templateUrl: './home-rahem.component.html',
       styleUrl: './home-rahem.component.css'
 })
-export class HomeRahemComponent {
+export class HomeRahemComponent implements OnInit {
       isSidebarOpen: boolean = false;
+      nodeCount = signal<number>(0);
+
+      private treeDataService = inject(TreeDataService);
+
+      ngOnInit(): void {
+            this.treeDataService.getTreeNodes(1).subscribe(nodes => {
+                  this.nodeCount.set(nodes.length - 1);
+            });
+      }
 
       toggleSidebar() {
             this.isSidebarOpen = !this.isSidebarOpen;
