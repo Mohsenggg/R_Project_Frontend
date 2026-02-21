8e5ce6d01bea595faa3fbf4d1d56136d44d2b1e1
Mohsenggg <Mohse.gamal@gmail.com>
2026-02-21 15:17:54 +0200

    improve home structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,9 @@ export const routes: Routes = [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', loadComponent: () => import('./features/auth/pages/login/login.component').then(c => c.LoginComponent) },
       { path: 'register', loadComponent: () => import('./features/auth/pages/register/register.component').then(c => c.RegisterComponent) },
-      { path: 'home', loadComponent: () => import('./features/intro/home/pages/home-rahem/home-rahem.component').then(c => c.HomeRahemComponent) },
       { path: 'tree', loadComponent: () => import('./features/tree/classic-view/pages/tree/tree.component').then(c => c.TreeComponent) },
 
       { path: 'vTree', loadComponent: () => import('./features/tree/dynamic-view/component/view-tree/view-tree.component').then(c => c.ViewTreeComponent) },
-      { path: 'home2', loadComponent: () => import('./features/intro/home/pages/home/home.component').then(c => c.HomeComponent) },
 
 
 
